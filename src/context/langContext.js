e10e13768d0fee8913d42833e603391295c9c744
DevLepsy <70275748+DevLepsy@users.noreply.github.com>
2022-01-22 22:25:19 +0100

    Update langContext

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -5,8 +5,12 @@ export const Context = createContext()
 const ContextProvider = props => {
     const [lang, setlang] = useState('EN')
 
+    const toggleLang = changeLang => {
+        setlang(changeLang)
+    }
+
     return (
-        <Context.Provider value={{lang}}>
+        <Context.Provider value={{lang, toggleLang}}>
             {props.children}
         </Context.Provider>
     )
